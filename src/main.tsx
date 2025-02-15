4b0365e1591f315f9b5aa82513d6e5713b7b148d
lanNo19 <anabdimali@gmail.com>
2025-02-16 04:25:36 +0500

    adding the main itself

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,8 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App initialDocUrl={null} />} />
-        <Route path="/document/:docId" element={<DocumentView docUrl={window.location.hash.slice(1)} />} />
+        <Route path="/document/:docId" element={<DocumentView />} />
+
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
